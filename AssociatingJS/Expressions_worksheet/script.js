55c5d0d04ffdb1a085713c3c278fcc1c6cd60ce0
Jenny Garcia <gjenny86@gmail.com>
2013-06-12 14:59:56 -0400

    "Dog Years" assignment for Expression Worksheet coding.

---
+++ AssociatingJS/Expressions_worksheet/script.js
@@ -0,0 +1,11 @@
+//Jenny Garcia
+//June 12, 2013
+//Expression Worksheet
+
+//Calculate how old Sparky is in dog years based on his actual age.
+var dogAging = 7;
+var sparkysAge = 3;
+//Multiply 7 and 3
+var dogAging = sparkysAge * 7
+//Print out Sparky's real age
+console.log("Sparky\'s real age in dog years is " + 7 * 3);
