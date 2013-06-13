dcd4d72c88f3668b385ee941419b5f21f2436355
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 17:52:01 -0400

    Added pixel width and height information. -Expressions_Industry assignment

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -4,3 +4,5 @@
 var imageWidth = 17.778
 var imageHeight = 14.222
 //To find out the PPI we not only need the image size in inches, but also in pixels.
+var pixelWidth = 1280
+var pixelHeight = 1025
