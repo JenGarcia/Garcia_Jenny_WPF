c2be201992c23d4810b37d4a7a22b08dd1c2ba3d
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 14:08:35 -0400

    Final Expressions_Personal coding. Moved incomeResult variable to line 10. Created a new variable "extraCash" in order to subtract income results and total expenses. In doing so, I had to recode my console log code.

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -3,11 +3,12 @@
 //Expressions Personal
 
 //Calculate how much income is brought home every month
-var weeklyPay = 400;
-var averageMonth = 4;
+var weeklyPay = 400; //Weekly pay check amount
+var averageMonth = 4; //Average number of weeks within a month
 var monthlyIncome = weeklyPay * averageMonth //Calculates this information by multiplying weeklypay and averagemonth to figure out how much income is brought home every month
 var result = "The average income a month is " + monthlyIncome + " dollars" //States monthly income result
+var incomeResult = 1600 //Total for monthly income
 var monthlyExpenses = [950, 250, 130, 125, 80] //Listing monthly expenses using an array expression
 var totalExpenses = monthlyExpenses[0] + monthlyExpenses[1] + monthlyExpenses[2] + monthlyExpenses[3] + monthlyExpenses[4] //Adding monthly expenses listed in the array
-var incomeResult = 1600
-console.log(incomeResult - totalExpenses)
+var extraCash = incomeResult - totalExpenses //Calculating remainding monthly amount after paying off living expenses. This is done by subtracting the 1600 in resulat and the total of monthly living expenses which is 1535
+console.log("My extra spending money at the end of every month is " + extraCash + " dollars.") //States total amount of extra spending money.
