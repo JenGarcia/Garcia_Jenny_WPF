d0a4b44bbeada7b88ce8bc2af4c7092cbad190ad
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 13:28:42 -0400

    Attempt number 3 in trying to get expenses subtracted from average monthly income. -Expressions_Personal assignment

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -9,5 +9,4 @@ var monthlyIncome = weeklyPay * averageMonth //Calculates this information by mu
 var result = "The average income a month is " + monthlyIncome + " dollars" //Calculates monthly income result
 var monthlyExpenses = [950, 250, 130, 125, 80] //Listing monthly expenses using an array expression
 var totalExpenses = monthlyExpenses[0] + monthlyExpenses[1] + monthlyExpenses[2] + monthlyExpenses[3] + monthlyExpenses[4] //Adding monthly expenses listed in the array
-var extraCash = totalExpenses - result
-console.log(extraCash)
+console.log("The amount left over after taking care of all living expenses every month is " + result - totalExpenses " dollars.")
