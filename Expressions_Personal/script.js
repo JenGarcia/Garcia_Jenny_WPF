29c2d9e13641af3cdc889607687323138c601e16
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 13:30:55 -0400

    Testing monthlyIncome (line 8) to make sure coding is correct. -Expressions Personal

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -6,7 +6,7 @@
 var weeklyPay = 400;
 var averageMonth = 4;
 var monthlyIncome = weeklyPay * averageMonth //Calculates this information by multiplying weeklypay and averagemonth to figure out how much income is brought home every month
-var result = "The average income a month is " + monthlyIncome + " dollars" //Calculates monthly income result
-var monthlyExpenses = [950, 250, 130, 125, 80] //Listing monthly expenses using an array expression
-var totalExpenses = monthlyExpenses[0] + monthlyExpenses[1] + monthlyExpenses[2] + monthlyExpenses[3] + monthlyExpenses[4] //Adding monthly expenses listed in the array
-console.log("The amount left over after taking care of all living expenses every month is " + result - totalExpenses " dollars.")
+//var result = "The average income a month is " + monthlyIncome + " dollars" //Calculates monthly income result
+//var monthlyExpenses = [950, 250, 130, 125, 80] //Listing monthly expenses using an array expression
+//var totalExpenses = monthlyExpenses[0] + monthlyExpenses[1] + monthlyExpenses[2] + monthlyExpenses[3] + monthlyExpenses[4] //Adding monthly expenses listed in the array
+console.log(monthlyIncome)
