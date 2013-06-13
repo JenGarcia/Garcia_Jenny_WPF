18d8e8faadf43265ebe803d756ae272f4b339de9
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 12:30:36 -0400

    Adding array variable. Assignment expressions personal

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -6,4 +6,5 @@
 var weeklyPay = 400;
 var averageMonth = 4;
 var monthlyIncome = weeklyPay * averageMonth //Calculates this information by multiplying weeklypay and averagemonth to figure out how much income is brought home every month
-var result = "The average income a month is " + monthlyIncome + " dollars"
+var result = "The average income a month is " + monthlyIncome + " dollars" //Calculates monthly income result
+var monthlyExpenses = ["950", "250", "130", "125", "80"]
