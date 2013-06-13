3711602aa3d6f009db53cec138c2e1755d301aa9
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 18:15:44 -0400

    Expressions_Industry. Not the result I was looking for :) Have to find a different way...

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -9,3 +9,4 @@ var pixelHeight = 1025
 //Once inch and pixel information is gathered, we now have to do some math!:)
 var widthInfo = pixelWidth / imageWidth //You must divide width in inches with pixel width
 var heightInfo = pixelHeight / imageHeight //Divide height in inches with pixel height
+console.log("The resolution for this image is " + widthInfo + heightInfo + " pixles per inch.")
