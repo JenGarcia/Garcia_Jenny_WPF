5a2d26402f915bd9ebd908f408188f479aec4d89
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 10:17:28 -0400

    "Expressions_Personal" assignment.

---
+++ Expressions_Personal/script.js
@@ -0,0 +1,9 @@
+//Jenny Garcia
+//June 13, 2013
+//Expressions Personal
+
+//Calculate how much income is brought home every month
+var weeklyPay = 400;
+var averageMonth = 4;
+var monthlyIncome = weeklyPay * averageMonth //Calculates this information by multiplying weeklypay and averagemonth to figure out how much income is brought home every month
+console.log("The average income a month is " + monthlyIncome + dollars)
