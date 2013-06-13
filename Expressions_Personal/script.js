c0f702fa251ce43d31595a89833730b0553352ee
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 13:07:55 -0400

    Testing array addition, attempt number 2. I realized that by adding quotes to each number listed in my array it was not adding it correctly. Rather than adding it, it just put it all in one line. After removing quotes the array line added correctly. Which came up with a total of 1535 dollars in expenses. Expression_Personal assignment

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -7,6 +7,6 @@ var weeklyPay = 400;
 var averageMonth = 4;
 var monthlyIncome = weeklyPay * averageMonth //Calculates this information by multiplying weeklypay and averagemonth to figure out how much income is brought home every month
 var result = "The average income a month is " + monthlyIncome + " dollars" //Calculates monthly income result
-var monthlyExpenses = ["950", "250", "130", "125", "80"] //Listing monthly expenses using an array expression
+var monthlyExpenses = [950, 250, 130, 125, 80] //Listing monthly expenses using an array expression
 var totalExpenses = monthlyExpenses[0] + monthlyExpenses[1] + monthlyExpenses[2] + monthlyExpenses[3] + monthlyExpenses[4] //Adding monthly expenses listed in the array
 console.log(totalExpenses)
