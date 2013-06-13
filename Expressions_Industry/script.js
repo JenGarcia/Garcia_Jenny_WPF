33292e94d4bf1be31de4e561c089fda0675c31c9
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 17:50:22 -0400

    Added image size (in inches) information. -Expressions-Industry

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -1,3 +1,6 @@
 //Jenny Garcia
 //June 13, 2013
 //Expressions Industry
+var imageWidth = 17.778
+var imageHeight = 14.222
+//To find out the PPI we not only need the image size in inches, but also in pixels.
