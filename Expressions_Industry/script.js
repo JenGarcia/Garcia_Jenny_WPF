9d8231e5410a304b9e74b7cbe52d868f956dec69
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 18:29:28 -0400

    Removed "0" from (heightInfo*10)/10 and changed it to just a "1" with no zeros. Rounding succesful :) Expressions Industry assignment

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -10,5 +10,5 @@ var pixelHeight = 1025
 var widthInfo = pixelWidth / imageWidth //You must divide width in inches with pixel width
 var heightInfo = pixelHeight / imageHeight //Divide height in inches with pixel height
 var widthResult = Math.round(widthInfo*10)/10
-var heightResult = Math.round(heightInfo*10)/10
+var heightResult = Math.round(heightInfo*1)/1
 console.log(heightResult)
