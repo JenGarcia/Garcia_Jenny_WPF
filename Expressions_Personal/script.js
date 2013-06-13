4eb7c8fffdc92fcbfb8a411b557c60831eac95f8
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 13:35:44 -0400

    Let's see if the new variable in line 12 will help :) for Expressions Personal assignment

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -6,7 +6,8 @@
 var weeklyPay = 400;
 var averageMonth = 4;
 var monthlyIncome = weeklyPay * averageMonth //Calculates this information by multiplying weeklypay and averagemonth to figure out how much income is brought home every month
-//var result = "The average income a month is " + monthlyIncome + " dollars" //Calculates monthly income result
-//var monthlyExpenses = [950, 250, 130, 125, 80] //Listing monthly expenses using an array expression
-//var totalExpenses = monthlyExpenses[0] + monthlyExpenses[1] + monthlyExpenses[2] + monthlyExpenses[3] + monthlyExpenses[4] //Adding monthly expenses listed in the array
+var result = "The average income a month is " + monthlyIncome + " dollars" //States monthly income result
+var monthlyExpenses = [950, 250, 130, 125, 80] //Listing monthly expenses using an array expression
+var totalExpenses = monthlyExpenses[0] + monthlyExpenses[1] + monthlyExpenses[2] + monthlyExpenses[3] + monthlyExpenses[4] //Adding monthly expenses listed in the array
+var incomeResult = 1600
 console.log(monthlyIncome)
