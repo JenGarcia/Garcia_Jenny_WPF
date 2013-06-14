e0a9d7dfaf063bbc5070231c87e3b93b1aa45150
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 21:44:30 -0400

    Added comment to line 7

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -4,7 +4,7 @@
 var start = prompt("If you had 12 slices of pizza and gave away 2. \nHow many would be left?") //alert message should appear on the web browser with this question
 var secondProblem = prompt("Correct, 10 is the right answer! \nNow what if you gave away 3 slices out of the 10. \nHow many would you have then?") //Second alert message pop-up
 var thirdProblem = prompt("Gasp! Spikey, the dog, just snatch 2 whole slices. \nHow many slices of pizza do you have left?") //Third alert message pop-up
-var fourthProblem = prompt("What? You don't like that name? \nWhat would you have named the dog?")
-alert("That's an interesting name :)")
+var fourthProblem = prompt("What? You don't like that name? \nWhat would you have named the dog?")//prompt will appear for you to fill in with answer
+alert("That's an interesting name :)") 
 var questionAgain = prompt("Back to math... \nSo now that you have 5 slices of pizza left. \nYou give away 1 more slice. \nHow many does that leave you with?")
 alert("You are correct! Give yourself a pat on the back")
