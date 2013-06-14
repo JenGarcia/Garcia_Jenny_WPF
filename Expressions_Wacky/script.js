7f0d507e1cbd9201d964c22a3bda64326d7dffd0
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 21:04:03 -0400

    Expressions_Wacky assignment: added prompt number 3

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -3,3 +3,4 @@
 //Expressions Wacky
 var start = prompt("If you had 12 slices of pizza and gave away 2. \nHow many would be left?") //alert message should appear on the web browser with this question
 var secondProblem = prompt("Correct, 10 is the right answer! \nNow what if you gave away 3 slices out of the 10. \nHow many would you have then?") //Second alert message pop-up
+var thirdProblem = prompt("Gasp! Spikey, the dog, just snatch 2 whole slices. \nHow many slices of pizza do you have left?")
