5a491defa7e935b5b2853db5894a5c5830feaa17
Jenny Garcia <gjenny86@gmail.com>
2013-06-13 21:01:12 -0400

    Expressions_Wacky assignment: Added 2 prompts with mathematical questions

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -1,4 +1,5 @@
 //Jenny Garcia
 //June 13, 2013
 //Expressions Wacky
-var start = prompt("If you had 12 slices of pizza and gave away 2. \nHow many would be left?")
+var start = prompt("If you had 12 slices of pizza and gave away 2. \nHow many would be left?") //alert message should appear on the web browser with this question
+var secondProblem = prompt("Correct, 10 is the right answer! \nNow what if you gave away 3 slices out of the 10. \nHow many would you have then?") //Second alert message pop-up
