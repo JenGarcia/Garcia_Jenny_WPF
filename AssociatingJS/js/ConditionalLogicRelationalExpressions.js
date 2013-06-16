dbe7f3c967102d973888c67eeef18d5aa4912110
Jenny Garcia <gjenny86@gmail.com>
2013-06-16 14:57:56 -0400

    Relational Expressions example: Comparing odjects for decision making.

---
+++ AssociatingJS/js/ConditionalLogicRelationalExpressions.js
@@ -0,0 +1,12 @@
+//Conditonal Logic Relational Expressions(Sample)
+
+var kidHeight = 40;
+var minHeight = 48;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+	//code performed if the condition is true
+	console.log("You can ride the coaster!");
+}
+
