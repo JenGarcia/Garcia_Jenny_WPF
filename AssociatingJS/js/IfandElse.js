a4395f030d809e3a75a2bb4bcad48dbb756ccb9e
Jenny Garcia <gjenny86@gmail.com>
2013-06-16 15:34:38 -0400

    If and Else: Choosing between 2 blocks of code. (practice example)

---
+++ AssociatingJS/js/IfandElse.js
@@ -0,0 +1,16 @@
+//If and Else(Sample)
+
+var kidHeight = 52;
+var minHeight = 48;
+//var sneakerLift = 2;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+	//code performed if the condition is true
+	console.log("You can ride the coaster!");
+}else{
+	//if condition is false
+//if(kidHeight <= minHeight)
+//console.log("Sorry kid, you've got some growing to do first!");
+}
