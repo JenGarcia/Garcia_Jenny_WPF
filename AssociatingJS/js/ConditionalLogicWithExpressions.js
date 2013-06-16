70f5e0c2c8854539d998e4ae3a6a6f463e996bea
Jenny Garcia <gjenny86@gmail.com>
2013-06-16 15:02:21 -0400

    Conditional Logic With an Expression example: Adding an expression with variable.

---
+++ AssociatingJS/js/ConditionalLogicWithExpressions.js
@@ -0,0 +1,13 @@
+//Conditonal Logic With an Expressions(Sample)
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift > minHeight){
+	//code performed if the condition is true
+	console.log("You can ride the coaster!");
+}
+
