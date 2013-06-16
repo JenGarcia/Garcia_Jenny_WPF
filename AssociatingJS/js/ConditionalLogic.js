b9c7cbca896947b270e6a52e7052b3b42c82208b
Jenny Garcia <gjenny86@gmail.com>
2013-06-16 14:35:40 -0400

    Practice Screencast for Week 3: Conditinials. The importance for proper tabbing for curly bracers.

---
+++ AssociatingJS/js/ConditionalLogic.js
@@ -0,0 +1,9 @@
+//Conditonal Logic (Sample)
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+	//code performed if the condition is true
+	console.log("You can ride the coaster!");
+}
