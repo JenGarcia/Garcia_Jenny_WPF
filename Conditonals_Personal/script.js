4e72ea862e8d35628374140e0d7f9fda8abea51e
Jenny Garcia <gjenny86@gmail.com>
2013-06-18 15:27:39 -0400

    Condtionals Personal: Added "else"

--- Conditonals_Personal/script.js
+++ Conditonals_Personal/script.js
@@ -2,9 +2,12 @@
 //June 16, 2013
 //Conditionals Personal
 
-var freeLanceEarnings = 300 //Average monthly earnings
-var newCar = 700 //Total car monthly payments including insurance
-var hourlyJobPay = 800 //Monthly earnings from part-time job
+var freeLanceEarnings = 300; //Average monthly earnings
+var newCar = 700; //Total car monthly payments including insurance
+var hourlyJobPay = 800; //Monthly earnings from part-time job
 //Code performed if the above statement is true
-if(freeLanceEarnings < newCar)
-	console.log("Sorry, you can't afford a car just yet.") //Prints out results to the console regarding statement
+if(freeLanceEarnings < newCar){
+	console.log("Sorry, you can't afford a car just yet."); //Prints out results to the console regarding statement
+}else{
+	
+}
