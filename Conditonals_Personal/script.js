95f3e4883fae9e9f1153995c8037b46aa8933762
Jenny Garcia <gjenny86@gmail.com>
2013-06-18 15:40:31 -0400

    Conditionals Personal: Changed "else" to "else if" and added action code as well as comment

--- Conditonals_Personal/script.js
+++ Conditonals_Personal/script.js
@@ -8,6 +8,6 @@ var hourlyJobPay = 800; //Monthly earnings from part-time job
 
 if(freeLanceEarnings < newCar){ //States the fact that freeLanceEarnings are lesser than newCar payments
 	console.log("Sorry, you can't afford a car just yet."); //Prints out results to the console regarding statement
-}else{
-	//if 
-}
+}else if(hourlyJobPay > newCar){ //Shows that hourlyjobpay is greather than car payments
+	//if hourlypay job is enough to make car payments "you can buy a new car!"
+}	
