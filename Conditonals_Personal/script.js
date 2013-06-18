8a309c43a06125cc260d8c1f798208ee9d37fd21
Jenny Garcia <gjenny86@gmail.com>
2013-06-18 16:24:24 -0400

    Conditionals Personal: After much observation and playing around with the code, I have discovered as to why it failed after the 2nd console testing. Line 9 needed some adjustments, in order for line 12 to function properly. Changed "<" to ">"

--- Conditonals_Personal/script.js
+++ Conditonals_Personal/script.js
@@ -4,10 +4,10 @@
 
 var freeLanceEarnings = 300; //Average monthly earnings
 var newCar = 700; //Total car monthly payments including insurance
-var hourlyJobPay = 800; //Monthly earnings from part-time job
+var monthlyJobEarnings = 800; //Monthly earnings from part-time job
 
-if(freeLanceEarnings < newCar){ //States the fact that freeLanceEarnings are lesser than newCar payments
+if(freeLanceEarnings > newCar){ //States the fact that freeLanceEarnings are lesser than newCar payments
 	console.log("Sorry, you can't afford a car just yet."); //Prints out results to the console regarding statement
-}else if(hourlyJobPay < newCar){ //Shows that hourlyjobpay is greather than car payments
+}else if(monthlyJobEarnings > newCar){ //Shows that hourlyjobpay is greather than car payments
 	console.log("Congratulations! You can now search for a new car."); //if hourlypay job is enough to make car payments "you can buy a new car!"
-}	
+}
