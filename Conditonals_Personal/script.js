12d88216ca8287750e168637a567ae32a6a30ab3
Jenny Garcia <gjenny86@gmail.com>
2013-06-18 16:27:58 -0400

    Conditionals Personal: Chnaged comment on console 2 to be a bit more descriptive.

--- Conditonals_Personal/script.js
+++ Conditonals_Personal/script.js
@@ -8,6 +8,6 @@ var monthlyJobEarnings = 800; //Monthly earnings from part-time job
 
 if(freeLanceEarnings > newCar){ //States the fact that freeLanceEarnings are lesser than newCar payments
 	console.log("Sorry, you can't afford a car just yet."); //Prints out results to the console regarding statement
-}else if(monthlyJobEarnings > newCar){ //Shows that hourlyjobpay is greather than car payments
-	console.log("Congratulations! You can now search for a new car."); //if hourlypay job is enough to make car payments "you can buy a new car!"
+}else if(monthlyJobEarnings > newCar){ //Shows that monthly earnings are greater than car payments
+	console.log("Congratulations! You can now search for a new car."); //prints to the console if condition is true. hourlypay job is enough to make car payments "you can buy a new car!"
 }
