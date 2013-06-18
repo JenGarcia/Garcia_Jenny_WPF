e08abb39df39f35a13652d76a74c5bf290e49a94
Jenny Garcia <gjenny86@gmail.com>
2013-06-18 15:34:50 -0400

    Conditionals Personal: Corrected/changed comment on "if" actions for line 9.

--- Conditonals_Personal/script.js
+++ Conditonals_Personal/script.js
@@ -5,9 +5,9 @@
 var freeLanceEarnings = 300; //Average monthly earnings
 var newCar = 700; //Total car monthly payments including insurance
 var hourlyJobPay = 800; //Monthly earnings from part-time job
-//Code performed if the above statement is true
-if(freeLanceEarnings < newCar){
+
+if(freeLanceEarnings < newCar){ //States the fact that freeLanceEarnings are lesser than newCar payments
 	console.log("Sorry, you can't afford a car just yet."); //Prints out results to the console regarding statement
 }else{
-	
+	//if 
 }
