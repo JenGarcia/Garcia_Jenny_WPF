39f8dd3e01079cc16be2a832c96e8a88d7b5d0fe
Jenny Garcia <gjenny86@gmail.com>
2013-06-18 15:18:35 -0400

    Conditonals Personal code testing for first set of variables

--- Conditonals_Personal/script.js
+++ Conditonals_Personal/script.js
@@ -4,3 +4,6 @@
 
 var freeLanceEarnings = 300 //Average monthly earnings
 var newCar = 700 //Total car monthly payments including insurance
+//Code performed if the above statement is true
+if(freeLanceEarnings < newCar)
+	console.log("Sorry, you can't afford a car just yet.")
