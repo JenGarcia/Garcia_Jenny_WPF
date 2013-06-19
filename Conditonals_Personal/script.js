2b805e2d3be50d52acdc54529244c4f46ce4ca9b
Jenny Garcia <gjenny86@gmail.com>
2013-06-19 17:26:31 -0400

    Conditionals_Personal: Switched up last console outcome.

--- Conditonals_Personal/script.js
+++ Conditonals_Personal/script.js
@@ -9,8 +9,8 @@ var carInsurance = 250; //Monthly car insurance fee
 
 if(freeLanceEarnings > newCar){ //States the fact that freeLanceEarnings are lesser than newCar payments
 	console.log("Sorry, you can't afford a car just yet."); //Prints out results to the console regarding statement
-}else if(carInsurance < freeLanceEarnings){
-	console.log("You can only afford car insurance with your freelance earnings.")
-}else if(monthlyJobEarnings < newCar){ //Shows that monthly earnings are greater than car payments
-	console.log("Congratulations! You can now search for a new car."); //prints to the console if condition is true. hourlypay job is enough to make car payments "you can buy a new car!"
+}else if(carInsurance > freeLanceEarnings){
+	console.log("You can only afford car insurance with your freelance earnings.");
+}else if(monthlyJobEarnings > newCar){ //Shows that monthly earnings are greater than car payments
+	console.log("You can only afford a new car, but without insurance."); //Prints to the console if condition is true. Hourlypay job is enough to make car payments "you can buy a new car!"
 }
