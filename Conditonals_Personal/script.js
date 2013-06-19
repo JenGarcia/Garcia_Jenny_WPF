8e7d2a1643e39d905fa882c569478d1f7faa7c36
Jenny Garcia <gjenny86@gmail.com>
2013-06-19 12:10:22 -0400

    Conditionals_Personal: Added new variable and changed up values.

--- Conditonals_Personal/script.js
+++ Conditonals_Personal/script.js
@@ -3,11 +3,10 @@
 //Conditionals Personal
 
 var freeLanceEarnings = 300; //Average monthly earnings
-var newCar = 700; //Total car monthly payments including insurance
-var monthlyJobEarnings = 800; //Monthly earnings from part-time job
+var newCar = 500; //Total car monthly payments
+var monthlyJobEarnings = 600; //Monthly earnings from part-time job
+var carInsurance = 250; //Monthly car insurance fee
 
 if(freeLanceEarnings > newCar){ //States the fact that freeLanceEarnings are lesser than newCar payments
 	console.log("Sorry, you can't afford a car just yet."); //Prints out results to the console regarding statement
-}else if(monthlyJobEarnings > newCar){ //Shows that monthly earnings are greater than car payments
-	console.log("Congratulations! You can now search for a new car."); //prints to the console if condition is true. hourlypay job is enough to make car payments "you can buy a new car!"
-}
+}else if(carInsurance > freeLanceEarnings){
