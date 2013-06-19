c35a9029e4d5838da5edbca85814be65a2354073
Jenny Garcia <gjenny86@gmail.com>
2013-06-19 12:12:05 -0400

    Conditionals_Personal: Switched around "else if" code with "else" to test out new variable coding.

--- Conditonals_Personal/script.js
+++ Conditonals_Personal/script.js
@@ -10,3 +10,7 @@ var carInsurance = 250; //Monthly car insurance fee
 if(freeLanceEarnings > newCar){ //States the fact that freeLanceEarnings are lesser than newCar payments
 	console.log("Sorry, you can't afford a car just yet."); //Prints out results to the console regarding statement
 }else if(carInsurance > freeLanceEarnings){
+
+}else(monthlyJobEarnings > newCar){ //Shows that monthly earnings are greater than car payments
+	console.log("Congratulations! You can now search for a new car."); //prints to the console if condition is true. hourlypay job is enough to make car payments "you can buy a new car!"
+}
