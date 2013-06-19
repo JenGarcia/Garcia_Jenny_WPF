ebbbb8a3d1bd22db763a6ffe43c645532113b561
Jenny Garcia <gjenny86@gmail.com>
2013-06-19 12:15:50 -0400

    Conditonals_Personal: Code testing failure with new code.

--- Conditonals_Personal/script.js
+++ Conditonals_Personal/script.js
@@ -9,8 +9,8 @@ var carInsurance = 250; //Monthly car insurance fee
 
 if(freeLanceEarnings > newCar){ //States the fact that freeLanceEarnings are lesser than newCar payments
 	console.log("Sorry, you can't afford a car just yet."); //Prints out results to the console regarding statement
-}else if(carInsurance > freeLanceEarnings){
-
+}else if(carInsurance < freeLanceEarnings){
+	console.log("You can only afford car insurance with your freelance earnings.");
 }else(monthlyJobEarnings > newCar){ //Shows that monthly earnings are greater than car payments
 	console.log("Congratulations! You can now search for a new car."); //prints to the console if condition is true. hourlypay job is enough to make car payments "you can buy a new car!"
 }
