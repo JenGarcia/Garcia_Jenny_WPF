e6adc09d91ccaeedb64183f10c9c6a6f7fe4a356
Jenny Garcia <gjenny86@gmail.com>
2013-06-19 22:09:27 -0400

    Conditionals_Industry: Second and third variable added.

--- Conditionals_Industry/script.js
+++ Conditionals_Industry/script.js
@@ -4,3 +4,5 @@
 
 alert("Everybody wants to live the good life. \nNot only obtaining your dream job, but \nalso getting paid for doing what you love.") //Alert pop-up with description stated in parentheses
 var graphicDesignerIncome = 45000
+var webDeveloperIncome = 80000
+var webDesignerIncome = 52000
