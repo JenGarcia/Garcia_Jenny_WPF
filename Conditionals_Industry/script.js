82c2a7ca694be36697433262efdfda21db49e076
Jenny Garcia <gjenny86@gmail.com>
2013-06-19 22:07:31 -0400

    Conditionals_Industry: Comment created for alert pop-up.

--- Conditionals_Industry/script.js
+++ Conditionals_Industry/script.js
@@ -2,5 +2,5 @@
 //June 19, 2013
 //Conditionals Industry
 
-alert("Everybody wants to live the good life. \nNot only obtaining your dream job, but \nalso getting paid for doing what you love.")
+alert("Everybody wants to live the good life. \nNot only obtaining your dream job, but \nalso getting paid for doing what you love.") //Alert pop-up with description stated in parentheses
 var graphicDesignerIncome = 45000
