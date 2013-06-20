10570d54c302817cdc31dc5b180b272931276e06
Jenny Garcia <gjenny86@gmail.com>
2013-06-20 19:55:18 -0400

    Conditionals_Wacky: Added a second alert.

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -4,3 +4,4 @@
 
 var start = prompt("What do you get when you add the words 'ice' and 'cream' together?")
 var alertMessage = alert("Correct! It's Ice Cream!")
+var secondAlert = alert("Don't you just love compound words? \nLet's look at an awesome example.")
