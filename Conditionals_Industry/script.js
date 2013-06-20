b76a0f4af85142ec3db021a8566babba0cf5cee9
Jenny Garcia <gjenny86@gmail.com>
2013-06-19 22:11:06 -0400

    Conditionals_Industry: Commented on variables.

--- Conditionals_Industry/script.js
+++ Conditionals_Industry/script.js
@@ -3,6 +3,6 @@
 //Conditionals Industry
 
 alert("Everybody wants to live the good life. \nNot only obtaining your dream job, but \nalso getting paid for doing what you love.") //Alert pop-up with description stated in parentheses
-var graphicDesignerIncome = 45000
-var webDeveloperIncome = 80000
-var webDesignerIncome = 52000
+var graphicDesignerIncome = 45000 //Average yearly income
+var webDeveloperIncome = 80000//Average yearly income
+var webDesignerIncome = 52000//Average yearly income
