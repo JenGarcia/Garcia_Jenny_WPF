8d2bf5d43a656aa9cabaa789163f1a6ec6162317
Jenny Garcia <gjenny86@gmail.com>
2013-06-19 22:03:43 -0400

    Conditionals_Industry: first variable added

--- Conditionals_Industry/script.js
+++ Conditionals_Industry/script.js
@@ -1,4 +1,6 @@
 //Jenny Garcia
 //June 19, 2013
 //Conditionals Industry
+
 alert("Everybody wants to live the good life. \nNot only obtaining your dream job, but \nalso getting paid for doing what you love.")
+var graphicDesignerIncome = 45000
