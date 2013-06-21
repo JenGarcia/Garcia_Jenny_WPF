eb83cef4c26ac26125ba1f80da14aa7a778a8f6a
Jenny Garcia <gjenny86@gmail.com>
2013-06-20 20:43:22 -0400

    Conditionals_Wacky: Commented on prompts/alerts

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -2,6 +2,6 @@
 //June 20, 2013
 //Conditionals Wacky
 
-var start = prompt("What do you get when you add the words 'ice' and 'cream' together?")
-var alertMessage = alert("Correct! It's Ice Cream!")
-var secondAlert = alert("Don't you just love compound words? \nLet's look at an awesome example.")
+var start = prompt("What do you get when you add the words 'ice' and 'cream' together?")//Welcome prompt fill-in
+var alertMessage = alert("Correct! It's Ice Cream!")//Message alert when information has been send through alert message
+var secondAlert = alert("Don't you just love compound words? \nLet's look at an awesome example.")//Alert message prompt
