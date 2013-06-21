b520f05eb9c781e144cf7922f5d041ee6433a25d
Jenny Garcia <gjenny86@gmail.com>
2013-06-20 22:09:09 -0400

    Conditionals_Wacky: Added comments to consoles.

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -10,10 +10,10 @@ var perfectAge = 18;
 
 
 if(minimumAge > perfectAge){
-	console.log("You cannot drive from 11pm to 6am. Unless accompanied by a license driver who is at least 21 years old.");
+	console.log("You cannot drive from 11pm to 6am. Unless accompanied by a license driver who is at least 21 years old.");//Prints results to the console if condition is true.
 }else if(acceptableAge > perfectAge){
-	console.log("You cannot drive between the hours of 1am to 5am. Unless accompanied by a license driver who is at least 21 years old.");
+	console.log("You cannot drive between the hours of 1am to 5am. Unless accompanied by a license driver who is at least 21 years old.");//Prints results to the console if condition is true.
 }else if(minimumAge && acceptableAge < perfectAge){
-	console.log("Congratulations! You are free to drive as you please. However, please remember to drive safely!!")
+	console.log("Congratulations! You are free to drive as you please. However, please remember to drive safely!!");//Prints results to the console if condition is true.
 }
 
