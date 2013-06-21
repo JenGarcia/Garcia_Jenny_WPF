afcaf5567fefe006494e72e85cf848e2c3ab94b4
Jenny Garcia <gjenny86@gmail.com>
2013-06-20 21:42:49 -0400

    Conditionals_Wacky: Last console code fixed (ternary code) and testing succeeded!

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -13,7 +13,7 @@ if(minimumAge > perfectAge){
 	console.log("You cannot drive from 11pm to 6am. Unless accompanied by a license driver who is at least 21 years old.");
 }else if(acceptableAge > perfectAge){
 	console.log("You cannot drive between the hours of 1am to 5am. Unless accompanied by a license driver who is at least 21 years old.");
-}else if(minimumAge < perfectAge > acceptableAge){
-	console.log("Congratulations! You are free to drive as you please. However, please remember to drive saefely!!")
+}else if(minimumAge && acceptableAge < perfectAge){
+	console.log("Congratulations! You are free to drive as you please. However, please remember to drive safely!!")
 }
 
