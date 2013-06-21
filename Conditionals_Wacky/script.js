8025168cd63ba4eeb3f9c2d562a6d1a05a68cfe1
Jenny Garcia <gjenny86@gmail.com>
2013-06-20 21:35:52 -0400

    Conditionals_Wacky: Last console log code failure...

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -11,7 +11,9 @@ var perfectAge = 18;
 
 if(minimumAge > perfectAge){
 	console.log("You cannot drive from 11pm to 6am. Unless accompanied by a license driver who is at least 21 years old.");
-}else if(acceptableAge < perfectAge){
+}else if(acceptableAge > perfectAge){
 	console.log("You cannot drive between the hours of 1am to 5am. Unless accompanied by a license driver who is at least 21 years old.");
+}else if(minimumAge < perfectAge > acceptableAge){
+	console.log("Congratulations! You are free to drive as you please. However, please remember to drive saefely!!")
 }
 
