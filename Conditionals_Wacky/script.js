aa94cea81fbe8903667156dcd94a09c68e26ffde
Jenny Garcia <gjenny86@gmail.com>
2013-06-20 20:55:34 -0400

    Conditionals_Wacky: (Do-over) Changed propt to alert and created a new welcoming message.

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -2,10 +2,4 @@
 //June 20, 2013
 //Conditionals Wacky
 
-var start = prompt("What do you get when you add the words 'ice' and 'cream' together?")//Welcome prompt fill-in
-var alertMessage = alert("Correct! It's Ice Cream!")//Message alert when information has been send through alert message
-var secondAlert = alert("Don't you just love compound words? \nLet's look at an awesome example.")//Alert message prompt
-
-var firstWord = Teen
-var secondWord = age
-	console.log(firstWord && secondWord)
+var start = alert("Driving... every teenagers dream. Unfortunately, \nit isn't something we can just do over night.")//Welcome prompt fill-in
