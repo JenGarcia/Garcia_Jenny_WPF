0d4f83e63805f47e5e89749b40cc70303a52f4e9
Jenny Garcia <gjenny86@gmail.com>
2013-06-20 20:52:24 -0400

    Conditionals_Wacky: Original idea was not the best. Do over!

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -5,3 +5,7 @@
 var start = prompt("What do you get when you add the words 'ice' and 'cream' together?")//Welcome prompt fill-in
 var alertMessage = alert("Correct! It's Ice Cream!")//Message alert when information has been send through alert message
 var secondAlert = alert("Don't you just love compound words? \nLet's look at an awesome example.")//Alert message prompt
+
+var firstWord = Teen
+var secondWord = age
+	console.log(firstWord && secondWord)
