d2c91ca5955ce053169943d62efecad8e1348a6f
Jenny Garcia <gjenny86@gmail.com>
2013-06-20 21:01:01 -0400

    Conditionals_Wacky: Added second alert with comment and changed comment on first alert.

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -2,4 +2,5 @@
 //June 20, 2013
 //Conditionals Wacky
 
-var start = alert("Driving... every teenagers dream. Unfortunately, \nit isn't something we can just do over night.")//Welcome prompt fill-in
+var start = alert("Driving... every teenagers dream. Unfortunately, \nit isn't something we can just do over night.")//Welcome alert
+var secondAlert = alert("ATTENTION: Not all states in the U.S. have the same law.")//Alert message pop-up
