4992c7c0e4ec1354b3eea0a51c339fde87c4efc7
Jenny Garcia <gjenny86@gmail.com>
2013-06-24 12:46:34 -0400

    Week 4 Functions: Function invoke

--- AssociatingJS/js/Functions.js
+++ AssociatingJS/js/Functions.js
@@ -1,7 +1,7 @@
 //Functions - Function Execution
 
 function outptMsg(){
-	console.log("Hello World");
+	console.log("Hello World");//things within curly brackets is what the "factory" will perform
 }
 
 function calcArea(){
@@ -10,3 +10,6 @@ function calcArea(){
 	var area = width * height;
 	console.log(area);
 }
+
+calcArea(); //invoke the function so that it can print out to the console
+calcArea(); //if stated more than once, the answer will appear in the console multiple times as well
