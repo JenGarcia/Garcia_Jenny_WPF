6f8d2d005f786170dfc91b6df674d603d2ba6b50
Jenny Garcia <gjenny86@gmail.com>
2013-06-24 13:06:09 -0400

    Week 4 Functions: Arguments and Parameters

---
+++ AssociatingJS/js/Arguments_Parameters.js
@@ -0,0 +1,15 @@
+//Functions - Parameters and Arguments
+
+/*calcArea(30, 20); //arguments - inputting info
+
+function calcArea(w, h){ //storage bins w=30, h=20. order is important!
+	var area = w * h;
+	console.log(area);
+}*/
+
+function dogYears(age){ //parentheses holds parameters
+	//var age = 4;
+	var dogYears = age * 7;
+	console.log("Sparky is" + dogYears + " years old")
+}
+dogYears(4); //passing value () arguments
