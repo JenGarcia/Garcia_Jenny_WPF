bc55e87d5438eb7e18c52719f0d8ba4878d2fb26
Jenny Garcia <gjenny86@gmail.com>
2013-06-25 13:53:13 -0400

    Functionals_Wacky: Fixed userInput prompt info.

--- Functionals_Wacky/script.js
+++ Functionals_Wacky/script.js
@@ -2,7 +2,7 @@
 //June 24, 2013
 //Functions Wacky
 
-var userInput = prompt("Hello! Today we will play with multiplications. \nWe will begin once you type in your name in the box below.")//Prompt message pops-up where you can put in info
+var userInput = prompt("Hello! Today we will play with multiplications. \nWe will begin once you type your name in the box below.")//Prompt message pops-up where you can put in info
 var welcomeAlert = alert("Nice to meet you! Let's get started, shall we?")//Alert message pop-up
 
 
