0386d18fa823c18cc2c03ba9abda9e709a06fc2e
Jenny Garcia <gjenny86@gmail.com>
2013-06-24 23:40:35 -0400

    Functionals_Wacky: Added welcoming alert.

--- Functionals_Wacky/script.js
+++ Functionals_Wacky/script.js
@@ -3,3 +3,4 @@
 //Functions Wacky
 
 var userInput = prompt("Hello! Today we will play with multiplications. \nWe will begin once you type in your name in the box below.")
+var welcomeAlert = alert("Nice to meet you! Let's get started, shall we?")
