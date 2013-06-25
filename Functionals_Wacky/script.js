4976e950fb7985b2947eda866dc297a7cbf5cb42
Jenny Garcia <gjenny86@gmail.com>
2013-06-24 23:51:04 -0400

    Functions_Wacky: Added comments to alert and prompt

--- Functionals_Wacky/script.js
+++ Functionals_Wacky/script.js
@@ -2,5 +2,5 @@
 //June 24, 2013
 //Functions Wacky
 
-var userInput = prompt("Hello! Today we will play with multiplications. \nWe will begin once you type in your name in the box below.")
-var welcomeAlert = alert("Nice to meet you! Let's get started, shall we?")
+var userInput = prompt("Hello! Today we will play with multiplications. \nWe will begin once you type in your name in the box below.")//Prompt message pops-up where you can put in info
+var welcomeAlert = alert("Nice to meet you! Let's get started, shall we?")//Alert message pop-up
