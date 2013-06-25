9364186dc57e593face195c67acab2f5067d4b5f
Jenny Garcia <gjenny86@gmail.com>
2013-06-25 00:48:38 -0400

    Functionals_Wacky: Deleted calcMultiplication to test another method.

--- Functionals_Wacky/script.js
+++ Functionals_Wacky/script.js
@@ -5,7 +5,6 @@
 var userInput = prompt("Hello! Today we will play with multiplications. \nWe will begin once you type in your name in the box below.")//Prompt message pops-up where you can put in info
 var welcomeAlert = alert("Nice to meet you! Let's get started, shall we?")//Alert message pop-up
 
-calcMultiplication(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 function multiplyMe(number){
 	var test = number * 5;
