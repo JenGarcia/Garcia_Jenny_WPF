2c3082b2cb9c367229db245d1585ea3bc1cd465c
Jenny Garcia <gjenny86@gmail.com>
2013-06-24 22:03:05 -0400

    Functions_Wacky: Added/up-dated info for prompt.

--- Functionals_Wacky/script.js
+++ Functionals_Wacky/script.js
@@ -2,4 +2,4 @@
 //June 24, 2013
 //Functions Wacky
 
-var userInput = prompt("Hello! Today we will play with multiplications.")
+var userInput = prompt("Hello! Today we will play with multiplications. \nWe will begin with you filling the box below with your name.")
