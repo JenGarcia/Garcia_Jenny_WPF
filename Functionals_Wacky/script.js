c1f7ecb83cd4cc08330d4c57c48c36e97fdc5994
Jenny Garcia <gjenny86@gmail.com>
2013-06-24 23:56:22 -0400

    Functions_Wacky: Added argument

--- Functionals_Wacky/script.js
+++ Functionals_Wacky/script.js
@@ -4,3 +4,5 @@
 
 var userInput = prompt("Hello! Today we will play with multiplications. \nWe will begin once you type in your name in the box below.")//Prompt message pops-up where you can put in info
 var welcomeAlert = alert("Nice to meet you! Let's get started, shall we?")//Alert message pop-up
+
+calcMultiplication(5);
