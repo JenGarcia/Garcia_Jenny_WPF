86362e98123800343e6854a8b2a91b9d3c6d8e00
Jenny Garcia <gjenny86@gmail.com>
2013-06-26 17:26:51 -0400

    Functionals_Industry: Added comments.

--- Functionals_Industry/script.js
+++ Functionals_Industry/script.js
@@ -2,10 +2,10 @@
 //June 26, 2013
 //Functionals Industry -- Freelance Charges for Web Design
 
-var first = timeCalc(120, 120, 60);
-var payment = calcPayment(65, 150, 120);
+var first = timeCalc(120, 120, 60);//Arguments needed to store in parameters. This is the time frame for each project in minutes.
+var payment = calcPayment(65, 150, 120);//Arguments needed to store in parameters. This is the fee for each project.
 
-function timeCalc(d, p, f){
+function timeCalc(d, p, f){//Parameters - Stores reusable information. D=Design, P=Programming and F=Flash
 	var firstTotal = d + p + f
 	return firstTotal;
 }
