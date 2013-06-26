6e8981e006b89ba68fb2f1dac373414bc627e4b4
Jenny Garcia <gjenny86@gmail.com>
2013-06-26 13:52:55 -0400

    Functionals_Personal: Added remainding comments to code.

--- Functionals_Personal/script.js
+++ Functionals_Personal/script.js
@@ -2,7 +2,7 @@
 //June 25, 2013
 //Functions Personal -- Bus or Car?
 
-var bus = calcTime(30, 20, 10);//arguments - info being sent to parameters. Waiting time=30, bus=20, walk=10.
+var bus = calcTime(30, 20, 10);//Arguments - info being sent to parameters. Waiting time=30, bus=20, walk=10.
 
 function calcTime(t, b, w){//Parameters - storing information.
 	var busTravel = t + b + w//Calclating information.
@@ -11,22 +11,23 @@ function calcTime(t, b, w){//Parameters - storing information.
 console.log("If you take public transportation, you will spend " + bus + " minutes on the bus, in order to get to your destination.")//Information that prints out to the console.
 
 
-var car = calcTravel(5, 15, 3);
+var car = calcTravel(5, 15, 3);//Arguments - info being sent to parameters "storage bins." Time getting gas=5, time driving=15, finding parking=3.
 
-function calcTravel(g, d, p){
-	var carTravel = g + d + p
-	return carTravel;
+function calcTravel(g, d, p){//Parameters - "storage bins." Stores information from arguments.
+	var carTravel = g + d + p//Calculating information that's stored within parameters.
+	return carTravel;//Spits out the calculated information from var out of the function.
 }
-console.log("However, if you decide to travel by a vehicle, you will arrive at your destination in " + car + " minutes.")
+console.log("However, if you decide to travel by a vehicle, you will arrive at your destination in " + car + " minutes.")//Prints information to the console.
 
-var busTime = 60;
-var carTime = 23;
+var busTime = 60;//Travel time on bus, 60 minutes.
+var carTime = 23;//Travel time using a car, 23 minutes.
 
-/*if(busTime > carTime){
-	console.log("You're better off traveling by car.");
+/*if(busTime > carTime){//If the time traveling on a bus takes longer than traveling by car, "You're better off traveling by car."
+	console.log("You're better off traveling by car.");//Information printed out to the console IF the statement is true.
 }else{
-	if(busTime < carTime)
-		console.log("Catch the bus if you want to get to your destination quickly!")
-}*/
+	if(busTime < carTime)//If the time traveling by car takes longer than traveling in a bus, "Catch the bus if you want to get to your destination quickly!"
+		console.log("Catch the bus if you want to get to your destination quickly!")//Information printed out to the console IF the statement is true.
+}*/ //used to close out code above so ternary can be added.
 
-(busTime > carTime) ? console.log("You're better off traveling by car.") : console.log("Catch the bus if you want to get to your destination quickly!")
+//Ternary code used to summarize IF and ELSE coding above.
+(busTime > carTime) ? console.log("You're better off traveling by car.") : console.log("Catch the bus if you want to get to your destination quickly!")//Two different console codes shortened. The one that is true will be printed out to the console.
