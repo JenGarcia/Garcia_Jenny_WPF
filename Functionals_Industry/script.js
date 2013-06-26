c219580792ac551adae1f72aac4cd1706d35d83c
Jenny Garcia <gjenny86@gmail.com>
2013-06-26 16:36:53 -0400

    Functionals_Industry: Rearranged some coding and var labeling in order to test. Testing was succesful.

--- Functionals_Industry/script.js
+++ Functionals_Industry/script.js
@@ -2,6 +2,11 @@
 //June 26, 2013
 //Functionals Industry -- Freelance Charges for Web Design
 
-var firstTimeVisit = (120, 120, 60);
-var firstPayment = (120, 150, 65);
+var first = timeCalc(120, 120, 60);
+//var firstPayment = (120, 150, 65);
 
+function timeCalc(d, p, f){
+	var firstTotal = d + p + f
+	return firstTotal
+}
+console.log(first)
