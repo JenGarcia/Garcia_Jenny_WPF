61654529a7cfde50ce64ae92ecb25934cdb13691
Jenny Garcia <gjenny86@gmail.com>
2013-06-26 16:27:36 -0400

    Functionals_Industry: Added first 2 variables/arguments.

--- Functionals_Industry/script.js
+++ Functionals_Industry/script.js
@@ -1,3 +1,7 @@
 //Jenny Garcia
 //June 26, 2013
-//Functionals Industry
+//Functionals Industry -- Freelance Charges for Web Design
+
+var firstTimeVisit = (120, 120, 60);
+var firstPayment = (120, 150, 65);
+
