9733a8ee8bc273b1e5a9ef5a765edc14100f3389
Jenny Garcia <gjenny86@gmail.com>
2013-06-26 14:04:51 -0400

    Functionals_Personal: Corrected minor typos.

--- Functionals_Personal/script.js
+++ Functionals_Personal/script.js
@@ -2,18 +2,18 @@
 //June 25, 2013
 //Functions Personal -- Bus or Car?
 
-var bus = calcTime(30, 20, 10);//Arguments - info being sent to parameters. Waiting time=30, bus=20, walk=10.
+var bus = calcTime(30, 20, 10);//Arguments - Info being sent to parameters. Waiting time=30, bus=20, walk=10.
 
-function calcTime(t, b, w){//Parameters - storing information.
-	var busTravel = t + b + w//Calclating information.
+function calcTime(t, b, w){//Parameters - Storing information from arguments.
+	var busTravel = t + b + w//Calclating information that's stored within parameters.
 	return busTravel;//Spits information out of function.
 }
 console.log("If you take public transportation, you will spend " + bus + " minutes on the bus, in order to get to your destination.")//Information that prints out to the console.
 
 
-var car = calcTravel(5, 15, 3);//Arguments - info being sent to parameters "storage bins." Time getting gas=5, time driving=15, finding parking=3.
+var car = calcTravel(5, 15, 3);//Arguments - Info being sent to parameters "storage bins." Time getting gas=5, time driving=15, finding parking=3.
 
-function calcTravel(g, d, p){//Parameters - "storage bins." Stores information from arguments.
+function calcTravel(g, d, p){//Parameters - "Storage Bins." Stores information from arguments.
 	var carTravel = g + d + p//Calculating information that's stored within parameters.
 	return carTravel;//Spits out the calculated information from var out of the function.
 }
